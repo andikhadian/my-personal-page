b48f0e9324899ab9ea27284e15b59db296ad5b8e
Andikha Dian Nugraha <andikha.dian1@gmail.com>
2021-03-28 00:33:51 +0700

    initial commit

---
+++ pages/projects.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="min-h-screen bg-black text-white p-10">
+      <div class="flex justify-between items center">
+        <h2 class="text-6xl font-bold">Projects</h2>
+        <nuxt-link to="/">
+            <svg class="w-16 h-16" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
+        </nuxt-link>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
