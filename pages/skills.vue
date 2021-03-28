61e323540684cb130701aa667ec8042cdddfc4ea
Andikha Dian Nugraha <andikha.dian1@gmail.com>
2021-03-28 07:38:40 +0700

    improved header

--- pages/skills.vue
+++ pages/skills.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="min-h-screen bg-black text-white p-10">
       <div class="flex justify-between items center">
-        <h2 class="text-6xl font-bold">Skills</h2>
+        <h2 class="text-header">Skills</h2>
         <nuxt-link to="/">
-            <svg class="w-16 h-16" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
+            <svg class="close-header" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
         </nuxt-link>
       </div>
   </div>
